8f22fdd3bf79e47a447a129cac94264ab868ab85
Raghav-Hebbar <raghavhebbar2468@gmail.com>
2021-04-04 14:18:20 +0530

    changed wheaterapp config

--- wheaterapp/settings.py
+++ wheaterapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wheater',
+    'wheater.apps.WheaterConfig',
 ]
 
 MIDDLEWARE = [
--- wheaterapp/urls.py
+++ wheaterapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('wheater.urls')),
+    path('wheater/', include('wheater.urls')),
 ]
