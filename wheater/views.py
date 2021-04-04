372b35991e8de0ea4ed660caaec340245d57e5ec
Raghav-Hebbar <raghavhebbar2468@gmail.com>
2021-04-04 18:30:00 +0530

    printed out api response

--- wheater/views.py
+++ wheater/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=44af8d57d2ae36fea4714cab2cce068c'
+    city = 'London'
+    city_wheater = requests.get(url.format(city))
+    print(city_wheater.text)
     return render(request, 'wheater/home.html')
