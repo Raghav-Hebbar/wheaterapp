b419b437cf577696dafa6958557bbce4d887119b
Raghav-Hebbar <raghavhebbar2468@gmail.com>
2021-04-07 20:26:17 +0530

    returned data from api

--- wheater/views.py
+++ wheater/views.py
@@ -5,6 +5,12 @@ import requests
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=44af8d57d2ae36fea4714cab2cce068c'
     city = 'London'
-    city_wheater = requests.get(url.format(city))
-    print(city_wheater.text)
+    city_wheater = requests.get(url.format(city)).json()
+    wheater = {
+        'city': city,
+        'temperature' : city_wheater['main']['temp'],
+        'decsription' : city_wheater['weather'][0]['description'],
+        'icon' : city_wheater['weather'][0]['icon']
+    }
+    context = {'wheater_data': wheater}
     return render(request, 'wheater/home.html')
