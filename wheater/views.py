d28eca4aab9557e81df282f2cb5e717baec57d8f
Raghav-Hebbar <raghavhebbar2468@gmail.com>
2021-04-17 10:38:52 +0530

    can store weatherdata of multiple cities

--- wheater/views.py
+++ wheater/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+#from .models import City
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=44af8d57d2ae36fea4714cab2cce068c'
-    city = 'Bangalore'
+    #cities = City.objects.all()
+    cities = ['London', 'Tokyo', 'Bangalore'] 
+    weather_data = []
 
-    city_wheater = requests.get(url.format(city)).json()
-    wheater = {
-        'city': city,
-        'temperature' : city_wheater['main']['temp'],
-        'decsription' : city_wheater['weather'][0]['description'],
-        'icon' : city_wheater['weather'][0]['icon'],
-        'country_code' : city_wheater['sys']['country'],
-        'humidity' : city_wheater['main']['humidity'],
-        'wind' : city_wheater['wind']['speed'],
-        'high': city_wheater['main']['temp_max'],
-        'low' : city_wheater['main']['temp_min'],
-    }
-    context = {'wheater_data': wheater}
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=44af8d57d2ae36fea4714cab2cce068c'
+    
+    for city in cities: 
+
+        city_wheater = requests.get(url.format(city)).json()
+
+        wheater = {
+            'city': city,
+            'temperature' : city_wheater['main']['temp'],
+            'decsription' : city_wheater['weather'][0]['description'],
+            'icon' : city_wheater['weather'][0]['icon'],
+            'country_code' : city_wheater['sys']['country'],
+            'humidity' : city_wheater['main']['humidity'],
+            'wind' : city_wheater['wind']['speed'],
+            'high': city_wheater['main']['temp_max'],
+            'low' : city_wheater['main']['temp_min'],
+        }
+        weather_data.append(wheater)
+         
+        print(weather_data)
+    context = {'wheater_data': weather_data}
 
     return render(request, 'wheater/main.html',context)
