39a80cda0444528aa4b67018ac5a8dba41233f17
Raghaveshwara Hebbar <62178313+Raghav-Hebbar@users.noreply.github.com>
2023-06-08 01:52:12 +0530

    Update settings.py

--- wheaterapp/settings.py
+++ wheaterapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7487*it9b0_jy=c54ovd!ky0jiyc6dzpn*^l87i*l+*_i^2)8d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
